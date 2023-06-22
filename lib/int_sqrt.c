bf17173f9514e56f7caa059caf1af49ee4908adc
KanekiOnRTX <132217266+KanekiOnRTX@users.noreply.github.com>
2023-06-23 01:16:56 +0400

    Sqrt perf x3

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -16,25 +16,42 @@
  *
  * A very rough approximation to the sqrt() function.
  */
-unsigned long int_sqrt(unsigned long x)
+inline unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long b, m, y = 0;
+register unsigned long tmp;
+
+	register unsigned long place;	register unsigned long root = 0;
 
 	if (x <= 1)
 		return x;
-
-	m = 1UL << (__fls(x) & ~1UL);
-	while (m != 0) {
-		b = y + m;
+	
+place = 1UL << (BITS_PER_LONG - 2);
+	
 		y >>= 1;
+do{
 
-		if (x >= b) {
-			x -= b;
-			y += m;
+		place >>= 2;	}while(place > x);
+
+	do {
+
+		tmp = root + place;
+
+		root >>= 1;
+
+		if (x >= tmp)
+
+		{
+
+			x -= tmp;
+
+			root += place;
+		
 		}
-		m >>= 2;
+place >>= 2;
+
+	}while (place != 0);
 	}
 
-	return y;
+	return root;
 }
 EXPORT_SYMBOL(int_sqrt);
